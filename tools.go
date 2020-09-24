27d5ab6c41721e461df8c6f378bb9eec6e10d283
ben <ben@cyber.casa>
2020-09-24 15:35:57 +0100

    Attempt to avoid import problems

--- tools.go
+++ tools.go
@@ -20,6 +20,4 @@ import (
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/protoc-gen-go"
-	_ "go.etcd.io/etcd"
-	_ "go.etcd.io/etcd/etcdctl"
 )
